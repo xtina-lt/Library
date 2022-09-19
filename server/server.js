4010920893378cab5787c5c8c927d9ec04c70115
xtina-lt <xtina_lt@outlook.com>
2022-09-19 18:40:25 -0400

    8000 back

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = 8000;
 
 app.use( express.json() )
 app.use( express.urlencoded({extended:true}) )
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:8000'}));
 app.use(cookieParser())
 
 // cors cross-origin requests
